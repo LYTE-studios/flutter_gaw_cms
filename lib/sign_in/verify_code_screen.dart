0b1dd8d20e38d8fd871e4566b50daad1436d366d
Bogdan Teodoru <a.teodoru8@gmail.com>
2024-01-18 18:52:24 +0100

    fully functional password reset screens

---
+++ lib/sign_in/verify_code_screen.dart
@@ -0,0 +1,145 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gaw_cms/core/routing/sign_in_router.dart';
+import 'package:flutter_gaw_cms/core/utils/exception_handler.dart';
+import 'package:gaw_api/gaw_api.dart';
+import 'package:gaw_ui/gaw_ui.dart';
+
+BeamPage verifyCodeBeamPage(String email) {
+  return BeamPage(
+    title: 'Verify Code',
+    key: const ValueKey('verify-code'),
+    type: BeamPageType.slideRightTransition,
+    child: VerifyCodeScreen(email: email),
+  );
+}
+
+class VerifyCodeScreen extends StatefulWidget {
+  const VerifyCodeScreen({super.key, required this.email});
+
+  static const String route = '/sign-in/welcome/forgot_password/verify_code';
+  final String email;
+
+  @override
+  State<VerifyCodeScreen> createState() => _VerifyCodeScreenState();
+}
+
+class _VerifyCodeScreenState extends State<VerifyCodeScreen>
+    with ScreenStateMixin {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  bool valid = false;
+
+  late final TextEditingController tecCode = TextEditingController()
+    ..addListener(validate);
+
+  void validate() {
+    bool newValid = tecCode.text.isNotEmpty;
+
+    if (newValid != valid) {
+      setState(() {
+        valid = newValid;
+      });
+    }
+  }
+
+  void onVerifyCode(ctx) {
+    if (!valid || loading) {
+      return;
+    }
+
+    setLoading(true);
+
+    AuthenticationApi.verifyPasswordResetCode(
+      request: CodeVerificationRequest(
+        (b) => b
+          ..email = widget.email
+          ..code = tecCode.text,
+      ),
+    ).then((value) {
+      setLoading(false);
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   const SnackBar(
+      //     behavior: SnackBarBehavior.floating,
+      //     backgroundColor: GawTheme.clearBackground,
+      //     content: Text('Code verified!', style: TextStyle(color: Colors.black87)),
+      //   ),
+      // );
+      print(value!.token);
+      signInRouter.beamToNamed(
+          '/sign-in/welcome/forgot-password/reset-password/${tecCode.text}/${value!.token}');
+    }).catchError((error) {
+      setLoading(false);
+
+      ExceptionHandler.show(
+        error,
+        context: context,
+        message: 'Are you sure the given code is correct?',
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: GawTheme.clearBackground,
+      body: Center(
+        child: SizedBox(
+          height: 480,
+          width: 320,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              HeaderBackButton(
+                color: GawTheme.text,
+                goBack: () => signInRouter.beamBack(),
+              ),
+              const SizedBox(
+                height: PaddingSizes.extraBigPadding,
+              ),
+              MainText(
+                'Reset your password',
+                alignment: TextAlign.start,
+                textStyleOverride: TextStyles.mainStyleTitle.copyWith(
+                  fontFamily: 'General Sans',
+                  fontWeight: FontWeight.w900,
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(
+                height: PaddingSizes.bigPadding,
+              ),
+              MainText(
+                'To verify your identity, please enter the code we sent to your email address.',
+                alignment: TextAlign.start,
+                textStyleOverride: TextStyles.mainStyleTitle.copyWith(
+                  color: Colors.black87.withOpacity(0.6),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 13,
+                ),
+              ),
+              const SizedBox(
+                height: PaddingSizes.extraBigPadding + 8,
+              ),
+              CmsInputField(
+                hint: '012345',
+                label: 'Code',
+                controller: tecCode,
+              ),
+              const SizedBox(
+                height: PaddingSizes.extraBigPadding,
+              ),
+              GenericButton(
+                label: 'Verify code',
+                onTap: !valid ? null : () => onVerifyCode(context),
+                loading: loading,
+                color: valid ? GawTheme.mainTint : GawTheme.unselectedMainTint,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
