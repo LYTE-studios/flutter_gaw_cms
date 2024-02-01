f52861acd3fb4e5107c1f69665157af9c0d80db2
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-02-01 01:14:48 +0100

    splash

--- lib/dashboard/pages/customers_page.dart
+++ lib/dashboard/pages/customers_page.dart
@@ -31,7 +31,11 @@ class _CustomersPageState extends State<CustomersPage> with ScreenStateMixin {
 
   CustomerListResponse? customerListResponse;
 
-  void loadData({int? page, int? itemCount}) {
+  List<Customer> selection = [];
+
+  bool allSelected = false;
+
+  void loadData({int? page, int? itemCount, String? term}) {
     setLoading(true);
 
     setData(() {
@@ -39,7 +43,11 @@ class _CustomersPageState extends State<CustomersPage> with ScreenStateMixin {
       itemCount = itemCount;
     });
 
-    CustomerApi.getCustomers(page: page, itemCount: itemCount).then((response) {
+    CustomerApi.getCustomers(
+      page: page,
+      itemCount: itemCount,
+      searchTerm: term,
+    ).then((response) {
       setData(() {
         customerListResponse = response;
       });
@@ -69,6 +77,7 @@ class _CustomersPageState extends State<CustomersPage> with ScreenStateMixin {
       extraActionButtonPadding: 156,
       actionWidget: ActionButton(
         label: 'Create new customer',
+        icon: PixelPerfectIcons.customAdd,
         onTap: () {
           DialogUtil.show(
             dialog: const CustomerCreateDialog(),
@@ -84,6 +93,9 @@ class _CustomersPageState extends State<CustomersPage> with ScreenStateMixin {
           loading: loading,
           title: LocaleKeys.customers.tr(),
           valueName: LocaleKeys.customers.tr().toLowerCase(),
+          onSearch: (String? value) {
+            loadData(page: 1, itemCount: itemCount, term: value);
+          },
           onEditItemCount: (int index) {
             loadData(itemCount: index, page: page);
           },
@@ -93,19 +105,51 @@ class _CustomersPageState extends State<CustomersPage> with ScreenStateMixin {
           page: page,
           itemsPerPage: itemCount,
           totalItems: customerListResponse?.total,
-          header: const BaseListHeader(
+          header: BaseListHeader(
+            selected: allSelected,
+            onUpdate: (bool? value) {
+              if (value == null) {
+                return;
+              }
+              setState(() {
+                if (value) {
+                  selection.addAll(customerListResponse?.customers ?? []);
+                  allSelected = true;
+                } else {
+                  selection = [];
+                  allSelected = false;
+                }
+              });
+            },
             items: {
-              'Name': ListUtil.lColumn,
+              'Customer name': ListUtil.lColumn,
               'Email': ListUtil.xLColumn,
-              'Phone': ListUtil.mColumn,
+              'Phone': ListUtil.lColumn,
+              'Hours': ListUtil.mColumn,
+              '': ListUtil.sColumn,
               'Company': ListUtil.mColumn,
-              '': ListUtil.xSColumn,
+              ' ': ListUtil.xSColumn,
             },
           ),
           rows: customerListResponse?.customers.map(
                 (customer) {
-                  return InkWell(
-                    onTap: () {
+                  return BaseListItem(
+                    selected: selection.contains(customer),
+                    onUpdate: (bool? value) {
+                      if (value == null) {
+                        return;
+                      }
+                      setState(() {
+                        allSelected = false;
+
+                        if (value) {
+                          selection.add(customer);
+                        } else {
+                          selection.remove(customer);
+                        }
+                      });
+                    },
+                    onSelected: () {
                       DialogUtil.show(
                         dialog: CustomerDetailDialog(
                           customerId: customer.id!,
@@ -113,25 +157,42 @@ class _CustomersPageState extends State<CustomersPage> with ScreenStateMixin {
                         context: context,
                       );
                     },
-                    child: BaseListItem(
-                      items: {
-                        TextRowItem(
-                          value: customer.getFullName(),
-                        ): ListUtil.lColumn,
-                        SelectableTextRowItem(
-                          value: customer.email,
-                        ): ListUtil.xLColumn,
-                        SelectableTextRowItem(
-                          value: customer.phoneNumber,
-                        ): ListUtil.mColumn,
-                        TextRowItem(
-                          value: customer.company,
-                        ): ListUtil.mColumn,
-                        const IconRowItem(
-                          icon: PixelPerfectIcons.eyeNormal,
-                        ): ListUtil.xSColumn,
-                      },
-                    ),
+                    items: {
+                      ProfileRowItem(
+                        firstName: customer.firstName,
+                        lastName: customer.lastName,
+                        initials: customer.initials,
+                        imageUrl: customer.profilePictureUrl,
+                      ): ListUtil.lColumn,
+                      SelectableTextRowItem(
+                        value: customer.email,
+                      ): ListUtil.xLColumn,
+                      SelectableTextRowItem(
+                        value: customer.phoneNumber,
+                      ): ListUtil.lColumn,
+                      const TextRowItem(
+                        value: '',
+                      ): ListUtil.mColumn,
+                      StatusRowItem(
+                        value: 'New',
+                        color: GawTheme.success,
+                        visible:
+                            GawDateUtil.tryFromApi(customer.createdAt)?.isAfter(
+                                  DateTime(
+                                    DateTime.now().year,
+                                    DateTime.now().month,
+                                    DateTime.now().day - 3,
+                                  ),
+                                ) ??
+                                false,
+                      ): ListUtil.sColumn,
+                      TextRowItem(
+                        value: customer.company,
+                      ): ListUtil.mColumn,
+                      const IconRowItem(
+                        icon: PixelPerfectIcons.eyeNormal,
+                      ): ListUtil.xSColumn,
+                    },
                   );
                 },
               ).toList() ??
--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -5,9 +5,9 @@ import 'package:flutter_gaw_cms/core/utils/exception_handler.dart';
 import 'package:flutter_gaw_cms/dashboard/dashboard_screen.dart';
 import 'package:flutter_gaw_cms/secrets.dart';
 import 'package:flutter_gaw_cms/sign_in/sign_in_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gaw_api/gaw_api.dart';
 import 'package:gaw_ui/gaw_ui.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 const BeamPage splashBeamPage = BeamPage(
   title: 'Get a Wash',
@@ -71,8 +71,9 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
       _goToDashboard();
       return;
     }
-
-    Future(() {
+    
+    Future(() async {
+      await PixelPerfectIcons.loadIcons();
       getTokens();
     });
     super.initState();
@@ -81,11 +82,10 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: GawTheme.background,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: GawTheme.mainTint,
       body: Center(
-        child: CircularProgressIndicator(
-          color: GawTheme.mainTint,
-        ),
+        child: SplashLogo(),
       ),
     );
   }
